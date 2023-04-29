cbc79d58cbe4199f957095ba2f892190cd08496c
mikey <mforest@protonmail.com>
2023-04-29 19:01:30 -0400

    Used on demand imports for vue-color

--- app.vue
+++ app.vue
@@ -5,6 +5,7 @@
 </template>
 
 
+<!--
 <script>
 import '@/assets/css/main.css'
 import VueColor from '@ckpack/vue-color';
@@ -17,5 +18,28 @@ export default {
 };
 
 </script>
+-->
+
+
+<script>
+import '@/assets/css/main.css'
+import { Chrome, create } from '@ckpack/vue-color';
+
+export default {
+  setup() {
+    const nuxtApp = useNuxtApp();
+    nuxtApp.vueApp.use(
+      create({
+        components: {
+          Chrome,
+        },
+      })
+    );
+  },
+};
+
+</script>
+
+
 
 <style></style>
