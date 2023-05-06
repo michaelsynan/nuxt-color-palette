a44cbcb1b41bbc4d9bc3e227e2d23cdca661409b
mikey <mforest@protonmail.com>
2023-05-06 10:50:11 -0400

    Changed URL

--- components/auth.vue
+++ components/auth.vue
@@ -1,7 +1,7 @@
 <script setup>
 
 const config = useRuntimeConfig()
-  const supabase = useSupabaseClient(config.public.supabaseURL, config.public.supabaseKey)
+  const supabase = useSupabaseClient('https://lyrqtmaueesenuuhozvn.supabase.co', config.public.supabaseKey)
 
   console.log('supaseURL runtimeconfig:' + config.public.supabaseURL)
 
