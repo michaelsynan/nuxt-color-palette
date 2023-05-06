23089d056d31570b0a9456a488c15e54f9e870ad
mikey <mforest@protonmail.com>
2023-05-05 22:33:05 -0400

    Working on redirect url

---
+++ pages/confirmation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <a :href="confirmationUrl">Click here to confirm</a>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+// Access the current route object
+const route = useRoute();
+
+// Extract the confirmation_url query parameter from the route object
+const confirmationUrl = route.query.confirmation_url;
+</script>
